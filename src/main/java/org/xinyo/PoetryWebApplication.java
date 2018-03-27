f416b62c8cf89292f81d1e15eac1c9526de9d9c8
chengxinyong <chengxinyong@139.com>
2018-03-27 15:06:27 +0800

    project build

---
+++ src/main/java/org/xinyo/PoetryWebApplication.java
@@ -0,0 +1,12 @@
+package org.xinyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoetryWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoetryWebApplication.class, args);
+	}
+}
---
+++ src/test/java/org/xinyo/PoetryWebApplicationTests.java
@@ -0,0 +1,16 @@
+package org.xinyo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PoetryWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
